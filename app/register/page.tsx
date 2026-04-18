9fef2d118dffd3c421a62b643234ab5d51574ec6
iwunduchuks <iwunduchuks@gmail.com>
2026-04-18 09:17:07 +0100

    feat: register page comming soon message

---
+++ app/register/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import style from "./page.module.css";
+
+export default function RegisterPage() {
+  return (
+    <div className={style.page}>
+      <div>
+        <h1>Don't Hold Your Breath, We Are Coming Soon</h1>
+        <p>
+          Just wait a little longer or send us a message at{" "}
+          <span>info@linguasprouts.ca</span> for more information
+        </p>
+      </div>
+    </div>
+  );
+}
