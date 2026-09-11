df8719248c292d56d3fa05f4c8240ef0376905a1
iwunduchuks <iwunduchuks@gmail.com>
2026-09-11 17:58:38 +0100

    chore: staging dvh checks

--- app/admin/(programs)/after-school-language/page.tsx
+++ app/admin/(programs)/after-school-language/page.tsx
@@ -157,11 +157,7 @@ const ClassList = () => {
         {filterOpen && (
           <Box
             borderRadius={"8px"}
-            height={
-              CSS.supports("calc(100dvh - 64px - 16px)")
-                ? "calc(100dvh - 64px - 16px)"
-                : "calc(100vh - 64px - 16px)"
-            }
+            height={"calc(100dvh - 64px - 16px)"}
             overflow={"auto"}
             position={"sticky"}
             top={`calc(64px + 8px)`}
