2058e51b7056ebd6a3d7dca1dad34dfa9d45d187
iwunduchuks <iwunduchuks@gmail.com>
2026-09-11 18:29:04 +0100

    feat: Connect filter component to hook

--- app/admin/(programs)/after-school-language/page.tsx
+++ app/admin/(programs)/after-school-language/page.tsx
@@ -97,16 +97,20 @@ const childListColumns = [
 ];
 
 const ClassList = () => {
+  const smallBreakpoint = useMediaQuery(useTheme().breakpoints.up("sm"));
   const [filterOpen, setFilterOpen] = useState(false);
   const [filters, setFilters] = useState<FilterValue>({});
-  const { data, isPending } = useListAfterSchoolRegs();
+
+  const { data, isPending } = useListAfterSchoolRegs({
+    ageGroup: filters["Age Group"],
+    language: filters["Language"],
+  });
+
   const children = data?.data.map((r) => ({
     id: r.id,
     ...r.child,
     schedule: r.schedule.map((s) => `${s.day} ${s.time}`).join(", "),
   }));
-
-  const smallBreakpoint = useMediaQuery(useTheme().breakpoints.up("sm"));
 
   const handleFilterButtonClick = () => {
     setFilterOpen(!filterOpen);
@@ -123,7 +127,7 @@ const ClassList = () => {
         justifyContent={"space-between"}
       >
         <Typography variant="caption">
-          Count:{" " + children?.length}
+          Count:{" " + (children?.length || 0)}
         </Typography>
 
         <PrimaryButton
