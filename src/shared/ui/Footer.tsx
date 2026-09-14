9de9a179035ab4649fa258bb5405852d2260cfa6
iwunduchuks <iwunduchuks@gmail.com>
2026-09-14 22:00:38 +0300

    feat: Added french immigration sprint ad and minor ui changes

--- src/shared/ui/Footer.tsx
+++ src/shared/ui/Footer.tsx
@@ -30,76 +30,8 @@ function FooterStyled() {
         },
       }}
     >
-      <Stack
-        gap={"48px"}
-        marginBottom={"60px"}
-        justifyContent={"space-between"}
-        sx={{
-          flexDirection: { xs: "column", md: "row" },
-          alignItems: { xs: "stretch", md: "center" },
-        }}
-      >
-        <Box maxWidth={"300px"}>
-          <Logo />
-          <Typography marginTop={"24px"} color="inherit">
-            Building the next generation of global communicators.
-          </Typography>
-        </Box>
-
-        <Stack gap={"16px"} marginRight={"100px"}>
-          <Typography variant="subtitle1" color="inherit">
-            Company
-          </Typography>
-
-          <Link href={"/about"}>
-            <Typography variant="body1" color="inherit">
-              About Us
-            </Typography>
-          </Link>
-
-          <Link href={"/"}>
-            <Typography variant="body1" color="inherit">
-              Courses
-            </Typography>
-          </Link>
-
-          <Link href={"/contact"}>
-            <Typography variant="body1" color="inherit">
-              Contact Us
-            </Typography>
-          </Link>
-        </Stack>
-      </Stack>
-
-      <Divider sx={{ borderColor: (theme) => theme.palette.ibmgrey[20] }} />
-      <Stack
-        gap={"16px"}
-        sx={{
-          padding: { xs: "16px 0px", md: "32px 0px" },
-          flexDirection: { xs: "column", md: "row-reverse" },
-          justifyContent: "space-between",
-        }}
-      >
-        <Link href="/admin">
-          <Typography variant="body1">Admin</Typography>
-        </Link>
-
-        <Typography variant="body1">
-          © 2026 LinguaSprouts Academy. All rights reserved.
-        </Typography>
-      </Stack>
-
       <Image
-        id="footer-illustration"
-        src={"/illustration6.svg"}
-        alt="illustration"
-        width={40}
-        height={40}
-        style={{ position: "absolute", top: 35 }}
-      />
-
-      <Image
-        id="footer-illustration"
+        id="footer-bg"
         src={"/footer-bg2.svg"}
         alt="illustration"
         width={1280}
@@ -111,8 +43,78 @@ function FooterStyled() {
           height: "auto",
           left: 0,
           right: 0,
-          zIndex: -1,
+          zIndex: 0,
         }}
+      />
+
+      <Box component={"div"} id="footer-contents" position={"relative"}>
+        <Stack
+          gap={"48px"}
+          marginBottom={"60px"}
+          justifyContent={"space-between"}
+          sx={{
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: { xs: "stretch", md: "center" },
+          }}
+        >
+          <Box maxWidth={"300px"}>
+            <Logo />
+            <Typography marginTop={"24px"} color="inherit">
+              Building the next generation of global communicators.
+            </Typography>
+          </Box>
+
+          <Stack gap={"16px"} marginRight={"100px"}>
+            <Typography variant="subtitle1" color="inherit">
+              Company
+            </Typography>
+
+            <Link href={"/about"}>
+              <Typography variant="body1" color="inherit">
+                About Us
+              </Typography>
+            </Link>
+
+            <Link href={"/"}>
+              <Typography variant="body1" color="inherit">
+                Courses
+              </Typography>
+            </Link>
+
+            <Link href={"/contact"}>
+              <Typography variant="body1" color="inherit">
+                Contact Us
+              </Typography>
+            </Link>
+          </Stack>
+        </Stack>
+
+        <Divider sx={{ borderColor: (theme) => theme.palette.ibmgrey[20] }} />
+        <Stack
+          gap={"16px"}
+          sx={{
+            padding: { xs: "16px 0px", md: "32px 0px" },
+            flexDirection: { xs: "column", md: "row-reverse" },
+            justifyContent: "space-between",
+          }}
+        >
+          <Link href="/admin">
+            <Typography variant="body1">Admin</Typography>
+          </Link>
+
+          <Typography variant="body1">
+            © 2026 LinguaSprouts Academy. All rights reserved.
+          </Typography>
+        </Stack>
+      </Box>
+
+      <Image
+        id="footer-illustration"
+        src={"/illustration6.svg"}
+        alt="illustration"
+        width={40}
+        height={40}
+        style={{ position: "absolute", top: 35 }}
       />
     </Box>
   );
